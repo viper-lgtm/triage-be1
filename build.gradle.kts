b35afd940648b3959739a2b0324e93afdeddd526
Martin Ve <martinvetsu@gmail.com>
2026-06-03 16:54:39 +0200

    ci: repair codeql

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     kotlin("jvm") version "2.3.21"
-    kotlin("plugin.spring") version "2.4.0"
+    kotlin("plugin.spring") version "2.3.21"
     id("org.springframework.boot") version "4.0.5"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("plugin.jpa") version "2.2.21"
+    kotlin("plugin.jpa") version "2.3.21"
 }
 
 group = "cz.martinvedra"
